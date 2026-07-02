3ea404125051df695365ae26d3471dedda88da9b
Кирилл <131317714+Morkamo@users.noreply.github.com>
2026-07-02 16:28:27 +0900

    Commit 6

--- js/script.js
+++ js/script.js
@@ -27,5 +27,45 @@ document.addEventListener('DOMContentLoaded', function() {
             })
         }
     // #endregion
-})
 
+    // #region My Projects Picture Change Color Animation
+        const myProjectsPictures = document.querySelectorAll('.about-my-projects-picture');
+
+        const shadowColors = [
+            'rgba(255, 0, 0, 1)',
+            'rgba(0, 255, 0, 1)',
+            'rgba(0, 150, 255, 1)',
+            'rgba(255, 165, 0, 1)',
+            'rgba(155, 89, 182, 1)',
+            'rgba(241, 196, 15, 1)'
+        ];
+
+        myProjectsPictures.forEach(function(picture) {
+            picture.addEventListener('picture-shaked', function() {
+                const sortedColors = shadowColors.filter(color => color !== picture.dataset.currentColor);
+                const randomIndex = Math.floor(Math.random() * sortedColors.length);
+                const randomColor = sortedColors[randomIndex];
+                picture.style.boxShadow = `0px 4px 10px ${randomColor}`;
+                picture.dataset.currentColor = randomColor;
+            });
+        });
+    // #endregion
+
+    // #region My Projects Picture Shaking Animation
+        const ProjectsPicture = document.querySelectorAll('.about-my-projects-picture');
+        if (ProjectsPicture) {
+            ProjectsPicture.forEach(function(picture) {
+                picture.addEventListener('click', function() {
+                    picture.classList.add('shaking');
+                })
+            })
+
+            ProjectsPicture.forEach(function(picture) {
+                picture.addEventListener('animationend', function() {
+                    picture.classList.remove('shaking');
+                    picture.dispatchEvent(new Event('picture-shaked'));
+                })
+            })
+        }
+    // #endregion
+})
